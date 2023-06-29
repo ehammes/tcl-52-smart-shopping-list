70cdcc02c5b544cfdc5a0e8b833cfcbb7d531c3e
Elizabeth <elisha.elizabeth@gmail.com>
2023-06-28 22:56:09 -0700

    Updated tailwind config with color scheme, font family, etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			display: ['Montserrat', 'sans-serif'],
+		},
+		backgroundSize: {
+			50: '50%',
+		},
+		extend: {
+			colors: {
+				green: {
+					primary: '#299740',
+					secondary: '#29c540',
+				},
+				orange: {
+					primary: '#FF7F00',
+				},
+			},
+			listStyleImage: {
+				checkmark: 'url("img/token_checkmark.png")',
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
