ba59cb1c93acde54ed561384a5c94ad3dd01e997
Elizabeth <elisha.elizabeth@gmail.com>
2023-06-28 23:02:32 -0700

    Added additional nav header links and added app logo

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,46 +1,65 @@
 import { Outlet } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faListDots } from '@fortawesome/free-solid-svg-icons';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHouse } from '@fortawesome/free-solid-svg-icons';
+// import { faListDots } from '@fortawesome/free-solid-svg-icons';
+// import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
-import './Layout.css';
+// import './Layout.css';
 
-export function Layout({ listToken }) {
+export function Layout({ listToken, handleCreateNewList }) {
 	return (
 		<>
-			<div className="Layout">
-				<header className="Layout-header">
-					<h1>Smart Shopping List</h1>
+			<div className="font-display">
+				{/* Header Nav */}
+				<header className="flex flex-row shadow-md bg-green-primary">
+					<div className="w-full">
+						<NavLink to="/">
+							<img
+								className="relative object-contain p-4 h-28"
+								src="img/groceries@512.png"
+								alt="logo"
+							/>
+						</NavLink>
+					</div>
+					<div className="flex items-center justify-end object-contain w-full pr-5 space-x-10 text-end shrink">
+						{listToken ? (
+							<>
+								<NavLink
+									className="font-semibold text-white hover:border-b-2"
+									to="list"
+								>
+									SHOPPING LIST
+								</NavLink>
+								<NavLink
+									className="font-semibold text-white hover:border-b-2"
+									to="add-item"
+								>
+									ADD ITEM
+								</NavLink>
+							</>
+						) : null}
+						<button
+							className="font-semibold text-right text-white hover:border-b-2"
+							onClick={handleCreateNewList}
+						>
+							CREATE NEW LIST
+						</button>
+						<NavLink
+							className="font-semibold text-white hover:border-b-2"
+							to="about"
+						>
+							EXPLORE LIST FEATURES
+						</NavLink>
+					</div>
 				</header>
-				<main className="Layout-main">
-					<Outlet />
-				</main>
-				{listToken ? (
-					<nav className="Nav">
-						<div className="buttonContainer leftNav">
-							<NavLink to="/" className="Nav-link">
-								<FontAwesomeIcon icon={faHouse} />
-								<span>Home</span>
-							</NavLink>
-						</div>
-						<div className="buttonContainer leftNav">
-							<NavLink to="/list" className="Nav-link">
-								<FontAwesomeIcon icon={faListDots} />
-								<span>List</span>
-							</NavLink>
-						</div>
-						<div className="buttonContainer rightNav">
-							<NavLink to="add-item" className="Nav-link">
-								<FontAwesomeIcon icon={faCartPlus} />
-								<span>Add Item</span>
-							</NavLink>
-						</div>
-					</nav>
-				) : (
-					''
-				)}
+
+				{/* Main Content */}
+				<div className="">
+					<main className="">
+						<Outlet />
+					</main>
+				</div>
 			</div>
 		</>
 	);
